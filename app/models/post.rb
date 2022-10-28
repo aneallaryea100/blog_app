0568840facc1abb5b195ea90b9fdfe53ca201ea5
Aneal Laryea <aneallaryea100@gmail.com>
2022-10-28 21:54:12 +0000

    Add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-  # after_save :update_posts_counter
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
