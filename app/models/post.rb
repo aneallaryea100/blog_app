a2bfcefdcfe4d6707254e46372bc5bbe02e1a6e5
Aneal Laryea <aneallaryea100@gmail.com>
2022-10-28 14:08:05 +0000

    Update post and comment class

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,9 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-  def update_post_counter
+  after_save :update_posts_counter
+
+  def update_posts_counter
     author.increment!(:posts_counter)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
+
   def most_recent_post
     post.order(created_at: DESC).limit(3)
   end
